2307fd64530782b98f679d5dd32dce288b70f194
Muhammed Sufiyan <148213204+SufiyanSheikh0321@users.noreply.github.com>
2024-12-21 10:25:39 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+                                   # String
+
+name: str = "Muhammad Sufiyan"
+print(name)
+
+                                  # Integer
+
+age: int = 16
+age = 17
+print(age)
+
+                                  # Boolean
+
+work: bool = True
+print(work)
